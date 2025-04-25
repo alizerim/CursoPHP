bb4feaf0be86ca70100218447ce80ad623221178
alizerim <129442833+alizerim@users.noreply.github.com>
2025-04-24 22:45:32 -0300

    Update enviar_consulta.php
    habia dejado la conexion de el servidor, Woops!

--- TP_Final/enviar_consulta.php
+++ TP_Final/enviar_consulta.php
@@ -5,7 +5,7 @@ $edad_form = $_POST["edad"];
 $mensaje_form = $_POST["mensaje"];
 
 //coneccion BD NpCK1rAD6Vc80lP 
-$conexion = mysqli_connect("sql310.infinityfree.com", "if0_38566138", "NpCK1rAD6Vc80lP", "if0_38566138_php_curso") or exit ("no se pudo conectar la BD");
+$conexion = mysqli_connect("localhost", "root", "", "php_curso") or exit ("no se pudo conectar la BD");
 mysqli_query($conexion, "INSERT INTO ejemplo_1 VALUES (DEFAULT,'$nombre_form', '$apellido_form', '$edad_form', '$mensaje_form' );   ");
 mysqli_close($conexion);
 header("location: contacto.php?ok");
